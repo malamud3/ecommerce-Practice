1709dc664ceff384414780f6a3537dd6313fda08
amir <malamud95@gmail.com>
2025-01-06 17:11:37 +0200

    Changes to be committed: 	modified:   src/components/Order/Order.jsx

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useActionState } from 'react';
 import CartContext from '../../store/CartContext';
 import currencyFormatter from '../../util/formatting';
 import Input from '../UI/Input';
@@ -19,13 +19,10 @@ function Order() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
-  const {
-    data,
-    isLoading: isSending,
-    error,
-    sendRequest,
-    clearData,
-  } = useHttp('http://localhost:3000/orders', requestConfig);
+  const { data, error, sendRequest, clearData } = useHttp(
+    'http://localhost:3000/orders',
+    requestConfig
+  );
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -42,13 +39,10 @@ function Order() {
     clearData();
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const fd = new FormData(event.target);
+  async function submitAction(prevState,fd) {
     const customerData = Object.fromEntries(fd.entries()); // { email: test@example.com }
 
-    sendRequest(
+    await sendRequest(
       JSON.stringify({
         order: {
           items: cartCtx.items,
@@ -57,6 +51,8 @@ function Order() {
       })
     );
   }
+
+  const [formState, fromAction , isSending] = useActionState(submitAction, null);
 
   let actions = (
     <>
@@ -89,7 +85,7 @@ function Order() {
 
   return (
     <Modal open={userProgressCtx.progress === 'order'} onClose={handleClose}>
-      <form onSubmit={handleSubmit}>
+      <form action={fromAction}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
@@ -109,9 +105,6 @@ function Order() {
   );
 }
 export default Order;
-
-
-
 
 // function Order() {
 //   const cartCtx = useContext(CartContext);
